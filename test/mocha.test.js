b312ca1ed19402ab179d8d86aacd47ed03b49876
Josh Beckman <email@andjosh.com>
2016-10-16 08:40:40 -0500

    Update tests for no configs
    Now testing for:
    - setting headers
    - incrementing hits
    - decrementing time remaining
    - setting reset times
    - setting default limits

--- test/mocha.test.js
+++ test/mocha.test.js
@@ -1,60 +1,91 @@
 var assert = require('chai').assert
 var request = require('supertest')
+var async = require('async')
+var defaults = require('../defaults')
 require('../example/app.js')
 
 describe('Mongo-Throttle', function () {
   // app.use('/api/', throttle())
-  describe('No Configs', function () {
-    it('Test', function (done) {
+  describe('With no configs,', function () {
+    it('should set headers', function (done) {
       request('localhost:3000')
         .get('/')
         .expect(200, function (err, res) {
           if (err) return done(err)
           assert.equal(err, null)
-          console.log(res.headers['x-rate-limit-limit'])
-          console.log(res.headers['x-rate-limit-remaining'])
-          console.log(res.headers['x-rate-limit-reset'])
-          assert.equal(true, true)
+          assert.ok(res.headers['x-rate-limit-limit'])
+          assert.ok(res.headers['x-rate-limit-remaining'])
+          assert.ok(res.headers['x-rate-limit-reset'])
           done()
         })
     })
-    it('Test2', function (done) {
-      request('localhost:3000')
-        .get('/')
-        .expect(200, function (err, res) {
-          if (err) return done(err)
-          assert.equal(err, null)
-          console.log(res.headers['x-rate-limit-limit'])
-          console.log(res.headers['x-rate-limit-remaining'])
-          console.log(res.headers['x-rate-limit-reset'])
-          assert.equal(true, true)
-          done()
-        })
+    it('should decrement headers/count', function (done) {
+      var remaining = 0
+      async.series([
+        function (done) {
+          request('localhost:3000')
+            .get('/')
+            .expect(200, function (err, res) {
+              if (err) return done(err)
+              assert.equal(err, null)
+              assert.ok(res.headers['x-rate-limit-limit'])
+              assert.ok(res.headers['x-rate-limit-remaining'])
+              assert.ok(res.headers['x-rate-limit-reset'])
+              remaining = parseInt(res.headers['x-rate-limit-remaining'], 10)
+              done()
+            })
+        },
+        function (done) {
+          request('localhost:3000')
+            .get('/')
+            .expect(200, function (err, res) {
+              if (err) return done(err)
+              assert.equal(err, null)
+              assert.ok(res.headers['x-rate-limit-remaining'])
+              assert.ok((remaining - 1).toString() === res.headers['x-rate-limit-remaining'])
+              done()
+            })
+        }
+      ], done)
     })
-    it('Test3', function (done) {
-      request('localhost:3000')
-        .get('/')
-        .expect(200, function (err, res) {
-          if (err) return done(err)
-          assert.equal(err, null)
-          console.log(res.headers['x-rate-limit-limit'])
-          console.log(res.headers['x-rate-limit-remaining'])
-          console.log(res.headers['x-rate-limit-reset'])
-          assert.equal(true, true)
-          done()
-        })
+    it('should decrement reset time', function (done) {
+      var reset = 0
+      async.series([
+        function (done) {
+          request('localhost:3000')
+            .get('/')
+            .expect(200, function (err, res) {
+              if (err) return done(err)
+              assert.equal(err, null)
+              assert.ok(res.headers['x-rate-limit-limit'])
+              assert.ok(res.headers['x-rate-limit-remaining'])
+              assert.ok(res.headers['x-rate-limit-reset'])
+              reset = parseInt(res.headers['x-rate-limit-reset'], 10)
+              done()
+            })
+        },
+        function (done) {
+          request('localhost:3000')
+            .get('/')
+            .expect(200, function (err, res) {
+              if (err) return done(err)
+              assert.equal(err, null)
+              assert.ok(res.headers['x-rate-limit-reset'])
+              assert.ok(reset > parseInt(res.headers['x-rate-limit-reset'], 10))
+              done()
+            })
+        }
+      ], done)
     })
-    it('Test4', function (done) {
+    it('should set default limit', function (done) {
       this.timeout(1000000)
       request('localhost:3000')
         .get('/')
         .expect(200, function (err, res) {
           if (err) return done(err)
           assert.equal(err, null)
-          console.log(res.headers['x-rate-limit-limit'])
-          console.log(res.headers['x-rate-limit-remaining'])
-          console.log(res.headers['x-rate-limit-reset'])
-          assert.equal(true, true)
+          assert.ok(res.headers['x-rate-limit-limit'])
+          assert.ok(defaults.rateLimit.max.toString() === res.headers['x-rate-limit-limit'])
           done()
         })
     })
